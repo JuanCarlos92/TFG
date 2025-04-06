54c425b5c73f01f1705cd46f7910f4b5fafbf331
Juan Carlos <juancarlosfilter@gmail.com>
2025-04-07 00:37:18 +0200

    responsive cardweapon - weapon - wikiweapon

--- Frontend/MonsterHunter/src/environments/environment.prod.ts
+++ Frontend/MonsterHunter/src/environments/environment.prod.ts
@@ -1,19 +1,19 @@
 export const environment = {
   production: true,
-  host: 'https://a439-185-185-55-7.ngrok-free.app',
-  //host: 'localhost',
-  //port: 8080,
+  // host: 'https://a439-185-185-55-7.ngrok-free.app',
+  host: 'localhost',
+  port: 8080,
   monster: '/api/monster',
   weapon: '/api/weapon',
   weaponTypeId: '/api/weapon/type',
   armor: '/api/armor',
-  armorSet:'/api/armor/set',
+  armorSet: '/api/armor/set',
   charm: '/api/charm',
   decoration: '/api/decoration',
   item: '/api/item',
   kinsect: '/api/kinsect',
   location: '/api/location',
-  quest:'/api/quest',
-  recoleccion:'/api/recoleccion',
-  tool:'/api/tool'
+  quest: '/api/quest',
+  recoleccion: '/api/recoleccion',
+  tool: '/api/tool'
 };
--- Frontend/MonsterHunter/src/environments/environment.ts
+++ Frontend/MonsterHunter/src/environments/environment.ts
@@ -4,22 +4,22 @@
 
 export const environment = {
   production: false,
-  host: 'https://a439-185-185-55-7.ngrok-free.app',
-  //host: 'localhost',
-  //port: 8080,
+  // host: 'https://a439-185-185-55-7.ngrok-free.app',
+  host: 'localhost',
+  port: 8080,
   monster: '/api/monster',
   weapon: '/api/weapon',
   weaponTypeId: '/api/weapon/type',
   armor: '/api/armor',
-  armorSet:'/api/armor/set',
+  armorSet: '/api/armor/set',
   charm: '/api/charm',
   decoration: '/api/decoration',
   item: '/api/item',
   kinsect: '/api/kinsect',
   location: '/api/location',
-  quest:'/api/quest',
-  recoleccion:'/api/recoleccion',
-  tool:'/api/tool'
+  quest: '/api/quest',
+  recoleccion: '/api/recoleccion',
+  tool: '/api/tool'
 };
 
 /*
