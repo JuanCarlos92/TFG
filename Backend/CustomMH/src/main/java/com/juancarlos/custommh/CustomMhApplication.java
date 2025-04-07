0e8fded35ac5d03066661f9d70d53df5df7e1d5d
Juan Carlos <juancarlosfilter@gmail.com>
2025-04-07 23:51:12 +0200

    create microservice custommh

---
+++ Backend/CustomMH/src/main/java/com/juancarlos/custommh/CustomMhApplication.java
@@ -0,0 +1,13 @@
+package com.juancarlos.custommh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomMhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomMhApplication.class, args);
+    }
+
+}
